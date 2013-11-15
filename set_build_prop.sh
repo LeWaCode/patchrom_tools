47c8d5a2a34207d36fd6767d8006b31f184d3970
jiangjiawen <jwjiang@lewatek.com>
2013-11-15 13:58:44 +0800

    get ro.lewa.device from build.prop when not set

--- set_build_prop.sh
+++ set_build_prop.sh
@@ -7,7 +7,13 @@ cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.v
                      | sed -e "s/ro\.lewa\.device=.*//" | sed -e "s/ro\.sys\.partner=.*//" \
                      | sed -e "s/ro\.lewa\.version=.*//" > $build_prop_file.new
 
-echo "ro.lewa.device=$2" >> $build_prop_file.new
+
+if [ $2 == "Unknown" ]; then
+    device=`cat $build_prop_file | grep ro.product.device= | sed -e "s/ro\.product\.device=//"`
+    echo "ro.lewa.device=$device" >> $build_prop_file.new
+else
+    echo "ro.lewa.device=$2" >> $build_prop_file.new
+fi
 echo "ro.sys.partner=$4" >>  $build_prop_file.new
 echo "ro.lewa.version=LeWa_$(date +%y.%m.%d)" >> $build_prop_file.new
 mv $build_prop_file.new $build_prop_file
