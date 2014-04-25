18310875f608bb22cf591615cadcbec8f2d48302
jiangjiawen <jwjiang@lewatek.com>
2014-04-25 10:47:40 +0800

    upload weekly update(20140425)

--- build_target_files.sh
+++ build_target_files.sh
@@ -105,9 +105,9 @@ function build_ota_package {
     echo "Build full ota package: $OUT_DIR/$OUT_ZIP_FILE"
     $OTA_FROM_TARGET_FILES -n -k $PORT_ROOT/build/security/testkey -o $TARGET_OTA_ASSERT_DEVICE $TARGET_FILES_ZIP $OUT_DIR/$OUT_ZIP_FILE
     if [ "$PARTNER" != "Lewa" ];then
-        cp $FULL_OTA_FILES_ZIP $FULL_OTA_PACKAGE
+        cp $OUT_DIR/$OUT_ZIP_FILE $FULL_OTA_PACKAGE
     else
-        cp $FULL_OTA_FILES_ZIP $LEWA_OTA_FULL_PACKAGE
+        cp $OUT_DIR/$OUT_ZIP_FILE $LEWA_OTA_FULL_PACKAGE
     fi
 }
 
